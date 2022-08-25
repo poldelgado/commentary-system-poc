a442c209c04b452d1b7881fc0c7ee0734622195a
Pablo Delgado <poldelgado@gmail.com>
2022-08-25 15:26:55 -0300

    Added comment migration, comment model, comment controller, comment store form request.

---
+++ app/Models/Comment.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    const MAX_LEVEL_COMMENTS = 3;
+
+    protected $fillable = [
+        'comment',
+        'parent_id',
+        'depth',
+    ];
+
+    protected $with = [
+        'replies',
+    ];
+
+    public function parent()
+    {
+        return $this->belongsTo(self::class, 'parent_id');
+    }
+
+    public function replies(): HasMany
+    {
+        return $this->hasMany(self::class,'parent_id');
+    }
+
+    public function depth()
+    {
+        return $this->depth >= self::MAX_LEVEL_COMMENTS - 1;
+    }
+}
